f80ad1e22c19c6b44f964d621b5a8b7a85cb5642
Huzaifa <muhammad.huzaifa@immcorporate.com>
2024-01-04 15:39:21 +0500

    implemented image loading progress bar

--- Instazoo/composeApp/src/commonMain/kotlin/di/AppModule.kt
+++ Instazoo/composeApp/src/commonMain/kotlin/di/AppModule.kt
@@ -6,7 +6,6 @@ import data.repository.HomeRepositoryImpl
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 import presentation.home.HomeScreenViewModel
 
--- Instazoo/composeApp/src/commonMain/kotlin/presentation/main/App.kt
+++ Instazoo/composeApp/src/commonMain/kotlin/presentation/main/App.kt
@@ -59,7 +59,7 @@ private fun RowScope.TabNavigationItem(tab: Tab) {
 
     BottomNavigationItem(
         selected = tabNavigator.current == tab,
-        onClick = { tabNavigator.current = tab },
+        onClick = { tabNavigator.current = tab},
         icon = {
             tab.options.icon?.let {
                 Icon(
